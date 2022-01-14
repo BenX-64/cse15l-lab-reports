ed790f45ad111a3762a5eff071bd0a266202d805
Ben <benjx32@gmail.com>
2022-01-13 16:39:02 -0800

    react setup

---
+++ src/MenuBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class MenuBar extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                sup
+            </div>
+        );
+    }
+}
+
+export default MenuBar;
